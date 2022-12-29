a3370d8741363df12994e70efe73a717f53456bd
Zenish-P <77645279+Zenish-P@users.noreply.github.com>
2022-12-28 21:37:59 -0600

    Initialize project using Create React App Contain some basic things such as nav bar and setting up react and bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import {ToDos} from './components/ToDos';
+import {Footer} from './components/Footer';
+// import ToDo from './components/ToDo';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header title = "My ToDos List" searchBar = {false}/>   
+    <ToDos/>
+    <Footer/>
+    </>
   );
 }
 
