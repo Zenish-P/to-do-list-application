5809514c2c4c20dca3e998e941fc88c07f281d1c
Zenish-P <77645279+Zenish-P@users.noreply.github.com>
2023-01-02 23:55:17 -0600

    header color fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,8 @@
 import React from 'react'
 
 export const Footer = () => {
-  // let footerStyle = {
-  //   position: "relative",
-  //   width: "100%",
-  //   top: "36vh",
-  // };
+ 
   return (
-    //style={footerStyle}
     <footer className="bg-dark text-light py-2" >
       <p className="text-center">    Copyright &copy; Zenish Patel </p>
     </footer>
